7bc13cb541f68ff50558d091bb38d5d9bd51b2fa
agpiermarini <piermarini.andrew@gmail.com>
2018-02-06 18:54:16 -0700

    completed decrypt runner

---
+++ lib/decrypt.rb
@@ -0,0 +1,14 @@
+require "pry"
+require "./lib/enigma"
+
+enigma  = Enigma.new
+input   = ARGV[0]
+output  = ARGV[1]
+key     = ARGV[2]
+date    = ARGV[3]
+
+message = File.read(input).chomp
+decrypted_text = enigma.decrypt(message, key, date)
+File.open(output, 'w') { |file| file.write(decrypted_text)}
+
+puts "Created '#{output}' with the key #{key} and date #{date}"
