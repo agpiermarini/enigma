bb5155674e09f9a4cb611f6325038194a28c72a7
Eliot Swank <eliotswank@gmail.com>
2018-02-07 13:42:52 -0700

    fix dictionary issues

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,10 +1,6 @@
 module Dictionary
 
-<<<<<<< HEAD
-  CHAR_MAP ={
-=======
   CHAR_MAP =     {
->>>>>>> 7bc13cb541f68ff50558d091bb38d5d9bd51b2fa
                    "A" => 1,
                    "B" => 2,
                    "C" => 3,
@@ -30,11 +26,7 @@ module Dictionary
                    "W" => 23,
                    "X" => 24,
                    "Y" => 25,
-<<<<<<< HEAD
-                   "Z" => 26
-=======
                    "Z" => 26,
->>>>>>> 7bc13cb541f68ff50558d091bb38d5d9bd51b2fa
                    "a" => 27,
                    "b" => 28,
                    "c" => 29,
@@ -92,12 +84,7 @@ module Dictionary
                    ":" => 81,
                    "/" => 82,
                    "?" => 83,
-<<<<<<< HEAD
-                   "|" => 84
-                 }
-=======
                    "|" => 84,
                    "\\" => 85
                   }
->>>>>>> 7bc13cb541f68ff50558d091bb38d5d9bd51b2fa
 end
