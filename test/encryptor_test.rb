e05b8b847ca1d7a93c6a172b4d58c823c6cc5b27
agpiermarini <piermarini.andrew@gmail.com>
2018-02-08 03:41:56 -0700

    fixed comma spacing in encryptor test file

--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -39,7 +39,7 @@ class EncryptorTest < Minitest::Test
   def test_it_can_find_encrypt_values
     e = Encryptor.new
     expected = [14, 41, 27, 16]
-    actual = e.encrypt_values([8,5,12,12], [6,36,15,4])
+    actual = e.encrypt_values([8, 5, 12, 12], [6, 36, 15, 4])
 
     assert_equal expected, actual
   end
@@ -47,7 +47,7 @@ class EncryptorTest < Minitest::Test
   def test_it_can_find_number_and_symbol_encrypt_values
     e = Encryptor.new
     expected = [6, 21, 74, 60]
-    actual = e.encrypt_values([85,70,59,56], [6,36,15,4])
+    actual = e.encrypt_values([85, 70, 59, 56], [6, 36, 15, 4])
 
     assert_equal expected, actual
   end
@@ -62,7 +62,7 @@ class EncryptorTest < Minitest::Test
   def test_it_can_find_all_encrypt_values
     e = Encryptor.new
     expected = [40, 67, 53, 42, 47, 14, 64, 45, 50, 74, 45]
-    actual = e.merge_new_encrypt_values("hello world", [6,36,15,4])
+    actual = e.merge_new_encrypt_values("hello world", [6, 36, 15, 4])
 
     assert_equal expected, actual
   end
@@ -94,9 +94,8 @@ class EncryptorTest < Minitest::Test
   def test_it_can_find_all_encrypt_values_with_symbols
     e = Encryptor.new
     expected = [40, 7, 53, 4, 59, 14, 64, 57, 50, 36, 8, 3, 69, 85]
-    actual = e.merge_new_encrypt_values("h3l\\0 w0r\\<| w", [6,36,15,4])
+    actual = e.merge_new_encrypt_values("h3l\\0 w0r\\<| w", [6, 36, 15, 4])
 
     assert_equal expected, actual
   end
-
 end
